fa38566ff16d7e57e45c0217af9dfc9d410c6bf2
ssavassava <sangwo98.kang@gmail.com>
2022-04-21 12:06:40 +0900

    https fix (2)

--- prod/backend/loadbalancer.tf
+++ prod/backend/loadbalancer.tf
@@ -4,7 +4,15 @@ resource "aws_security_group" "allow_alb" {
   vpc_id      = data.aws_vpc.vpc.id
 
   ingress {
-    description = "ALB from VPC"
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from VPC"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
