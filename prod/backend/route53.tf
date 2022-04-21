db333242f37747ceb2a47bbf9925ffaaf788a1a8
ssavassava <sangwo98.kang@gmail.com>
2022-04-21 11:27:37 +0900

    route53 add

---
+++ prod/backend/route53.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "zone" {
+  name         = "goormedu-clone.com"
+  private_zone = false
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "api.goormedu-clone.com"
+  type    = "A"
+  ttl     = "300"
+  record  = [aws_lb.goormedu-clone-alb.dns_name]
+}
