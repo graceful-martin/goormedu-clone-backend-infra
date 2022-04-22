72aaf6e07346eff7bfcebf153d9eeee8d85aaa19
ssavassava <sangwo98.kang@gmail.com>
2022-04-22 14:48:56 +0900

    ec2 fix

--- prod/backend/loadbalancer.tf
+++ prod/backend/loadbalancer.tf
@@ -55,7 +55,7 @@ resource "aws_lb_listener" "redirect-listener" {
 }
 
 data "aws_acm_certificate" "issued" {
-  domain   = "api.goormedu-clone.com"
+  domain   = "*.goormedu-clone.com"
   statuses = ["ISSUED"]
 }
 
