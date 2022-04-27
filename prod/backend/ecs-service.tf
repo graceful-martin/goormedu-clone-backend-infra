6891397e7c4a2f0298c25b578dfd4ebb1e3a220a
ssavassava <sangwo98.kang@gmail.com>
2022-04-27 12:48:17 +0900

    vpc endpoint, service auto scailing fix

--- prod/backend/ecs-service.tf
+++ prod/backend/ecs-service.tf
@@ -57,7 +57,7 @@ resource "aws_ecs_service" "goormedu-clone-service" {
 resource "aws_appautoscaling_target" "ecs_target" {
   max_capacity       = 4
   min_capacity       = 1
-  resource_id        = "service/clusterName/serviceName"
+  resource_id        = "service/${aws_ecs_cluster.cluser.name}/${aws_ecs_service.goormedu-clone-service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
