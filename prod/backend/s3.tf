6509bf853914967c68d5cfbb284206c0e8580cba
ssavassava <sangwo98.kang@gmail.com>
2022-04-28 10:25:39 +0900

    s3 rollback

--- prod/backend/s3.tf
+++ prod/backend/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
                 "s3:DeleteObject",
                 "s3:GetObject"
             ],
-            "Resource": "arn:aws:s3:::goormedu-clone-bucket/*"
+            "Resource": "arn:aws:s3:::goormedu-clone-data-bucket/*"
         }
     ]
 }
@@ -40,5 +40,5 @@ resource "aws_s3_bucket_public_access_block" "bucket-public-access-block" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "goormedu-clone-bucket"
+  bucket = "goormedu-clone-data-bucket"
 }
